34131a0e53490c39dda5fa5965abff68311aaf18
lucas.lvn <lucas.lvn97439@gmail.com>
2023-02-23 05:33:23 +0100

    Ajout des entité images & prediction pour la migration

--- database/database.py
+++ database/database.py
@@ -6,11 +6,13 @@ from utils          import getConfig
 engine  = create_engine('postgresql://' + getConfig('user') + ':' + getConfig('password') + '@localhost/' + getConfig('database') + '', echo=True)
 Session = sessionmaker(bind=engine)
 
-from entity._model        import Models, Base
-from entity.user          import Users, Base
+from entity._model        import Models,  Base
+from entity.user          import Users,   Base
 from entity.classes       import Classes, Base
+from entity.images        import Images,  Base
 from entity.trained_model import TrainedModel, Base
-from entity.trained_on    import TrainedOn, Base
+from entity.prediction    import Predictions,  Base
+from entity.trained_on    import TrainedOn,    Base
 
 ''' Create all table'''
 Base.metadata.create_all(engine)
