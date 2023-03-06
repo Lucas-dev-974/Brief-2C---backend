e63406cb8607b5db8f0288a53fe6e8acaffe409d
Quentin0312 <112090824+Quentin0312@users.noreply.github.com>
2023-03-06 15:37:52 +0400

    ajout commentaire

--- controllers/stats_globales.py
+++ controllers/stats_globales.py
@@ -5,5 +5,6 @@ from database.database import session
 
 @hug.get('/total_pred')
 def recup_stats_globales():
-    totalPred = session.query(Predictions).count()
-    return {'total_predictions': totalPred}
+    totalPred = session.query(Predictions).count() # Nb total de prédictions
+    mauvaisePred = session.query(Predictions.id).where(Predictions.classe == None ).count() # Nb de mauvaises preds
+    return {'total_predictions': totalPred, 'mauvaises_predictions': mauvaisePred}
