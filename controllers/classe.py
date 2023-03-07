f3caab3ddbb87ab969f78b81456919da59e4e60d
Quentin0312 <112090824+Quentin0312@users.noreply.github.com>
2023-03-07 15:11:08 +0400

    ajout restriction token

--- controllers/classe.py
+++ controllers/classe.py
@@ -1,10 +1,11 @@
 from database.database import session
 from database.entity import  Classes
 from utils import toJson
+from controllers.authentification import token_key_authentication
 
 import hug
 
-@hug.get('/all')
+@hug.get('/all', requires=token_key_authentication)
 def getClasses():
     classes  = session.query(Classes).all()
     return toJson(classes, Classes)
