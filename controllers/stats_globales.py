e0fac402d63736d1064d60408aa28aeee02bcefa
Quentin0312 <112090824+Quentin0312@users.noreply.github.com>
2023-03-09 14:59:30 +0400

    fix graph pie stats globales

--- controllers/stats_globales.py
+++ controllers/stats_globales.py
@@ -6,5 +6,5 @@ from database.database import session
 @hug.get('/total_pred')
 def recup_stats_globales():
     totalPred    = session.query(Predictions).count() # Nb total de prédictions
-    mauvaisePred = session.query(Predictions.id).where(Predictions.classe_id == None ).count() # Nb de mauvaises preds
+    mauvaisePred = session.query(Predictions).where(Predictions.user_feedback == None ).count() # Nb de mauvaises preds
     return {'total_predictions': totalPred, 'mauvaises_predictions': mauvaisePred}
