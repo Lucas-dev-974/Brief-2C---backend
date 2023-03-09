32910c0547c382236aa1bc1b2a68bbca40345b58
lucas.lvn <lucas.lvn97439@gmail.com>
2023-03-09 10:49:19 +0100

    code reivew

--- controllers/stats_globales.py
+++ controllers/stats_globales.py
@@ -5,6 +5,6 @@ from database.database import session
 
 @hug.get('/total_pred')
 def recup_stats_globales():
-    totalPred = session.query(Predictions).count() # Nb total de prédictions
-    mauvaisePred = session.query(Predictions.id).where(Predictions.classe == None ).count() # Nb de mauvaises preds
+    totalPred    = session.query(Predictions).count() # Nb total de prédictions
+    mauvaisePred = session.query(Predictions.id).where(Predictions.classe_id == None ).count() # Nb de mauvaises preds
     return {'total_predictions': totalPred, 'mauvaises_predictions': mauvaisePred}
