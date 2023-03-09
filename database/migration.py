7a59053b936cdda9da24f508d3da3e3d1e53b7bf
Quentin0312 <112090824+Quentin0312@users.noreply.github.com>
2023-03-09 08:04:11 +0400

    ajout des tables loss et accuracy

--- database/migration.py
+++ database/migration.py
@@ -8,6 +8,8 @@ from entity.images        import Images,  Base
 from entity.trained_model import TrainedModel, Base
 from entity.prediction    import Predictions,  Base
 from entity.trained_on    import TrainedOn,    Base
+from entity.loss import Loss, Base
+from entity.accuracy import Accuracy, Base
 
 ''' Create all table'''
 Base.metadata.create_all(engine)
